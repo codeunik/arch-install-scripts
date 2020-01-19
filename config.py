5de12b38cacb16fa8cdcee8d092d6f23907e2c9c
Partha Ghosh <partha31415@gmail.com>
2020-01-19 23:16:59 +0530

    adding config file

---
+++ config.py
@@ -0,0 +1,10 @@
+efi="/dev/sda1"
+root="/dev/sda2"
+home="/dev/sda3"
+swap="/dev/sda4"
+swapfile=False
+cpu="intel"
+bootloader=1 # 1 for systemd-boot, 2 for GRUB
+hostname='jarvis'
+username='partha'
+
